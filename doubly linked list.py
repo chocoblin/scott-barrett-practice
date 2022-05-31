ee8bb559f39c62fe0f4fc4c91caaf20cd7a99788
chocoblin <crusader.jsr@gmail.com>
2022-05-31 19:48:26 +0530

    initialization and print_list method

---
+++ doubly linked list.py
@@ -0,0 +1,22 @@
+class Node:
+    def __init__ (self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
+
+class DoublyLinkedList:
+    def __init__ (self, value):
+        new_node = Node (value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+        print(str(self.head.value) + str(self.tail.value)+"\n")
+
+my_doubly_linked_list = DoublyLinkedList(6)
+my_doubly_linked_list.print_list()
