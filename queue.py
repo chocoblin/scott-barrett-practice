749fdbdad449e1cad24dd890863492ba30b11bd3
chocoblin <crusader.jsr@gmail.com>
2022-06-11 17:44:05 +0530

    added dequeue method

--- queue.py
+++ queue.py
@@ -29,8 +29,30 @@ class Queue:
             self.last = new_node
         self.length+=1
 
+    def dequeue(self):
+        if self.length==0:
+            return None
+        temp = self.first
+        if self.length==1:
+            self.first = None
+            self.last = None
+        else:
+            self.first = self.first.next
+            temp.next = None
+
+        self.length-=1
+        return temp
+
+
 my_queue = Queue(1) 
 my_queue.print_queue()
 
 my_queue.enqueue(2)
+my_queue.print_queue()
+
+my_queue.dequeue()
+my_queue.print_queue()
+my_queue.dequeue()
+my_queue.print_queue()
+my_queue.dequeue()
 my_queue.print_queue()
