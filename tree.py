55593581353c304bc577918df576fe21a4f98394
chocoblin <crusader.jsr@gmail.com>
2022-06-13 02:11:06 +0530

    initialization and insert method for a number

---
+++ tree.py
@@ -0,0 +1,50 @@
+from hashlib import new
+from tkinter.messagebox import NO
+from numpy import MAY_SHARE_EXACT
+
+from sympy import N
+
+
+class Node:
+    def __init__ (self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+class BinarySearchTree:
+    def __init__ (self):
+        self.root = None
+    
+    def insert(self, value):
+        new_node = Node(value)
+        if self.root is None:
+            self.root = new_node
+            return True
+        
+        temp = self.root
+        
+        while (True):
+            if new_node.value == temp.value:
+                return False
+            
+            if new_node.value < temp.value:
+                if temp.left is None:
+                    temp.left = new_node
+                    return True
+                temp = temp.left
+            
+            else:
+                if temp.right is None:
+                    temp.right = new_node
+                    return True
+                temp = temp.right
+
+
+my_tree = BinarySearchTree()
+my_tree.insert(2)
+my_tree.insert(1)
+my_tree.insert(3)
+
+print(my_tree.root.value)
+print(my_tree.root.left.value)
+print(my_tree.root.right.value)
